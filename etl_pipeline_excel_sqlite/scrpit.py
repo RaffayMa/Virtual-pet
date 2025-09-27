da5f8e37298ddd71e753dfcf8a203e047b599912
RaffayMa <muhammadraffay354@gmail.com>
2025-09-27 17:30:27 -0400

    Delete etl_pipeline_excel_sqlite directory

--- etl_pipeline_excel_sqlite/scrpit.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the data from the csv
-df = pd.read_csv('/Users/muhammadraffaymagoon/Desktop/Projects/etl_pipeline_excel_sqlite/messy-data.csv')
-
-# Examine shape
-print(df.head())
-
